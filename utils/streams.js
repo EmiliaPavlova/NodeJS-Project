dfe5b8632f572bda158da4deda2226b1c4b68d30
Emilia Pavlova <emilia.pavlova@gmail.com>
2018-05-22 00:30:14 +0300

    read action and file

--- utils/streams.js
+++ utils/streams.js
@@ -24,90 +24,57 @@ const program = require('commander');
 };
 */
 
-let getAction = (action, file) => {
-    console.log(action);
-    // switch(action) {
-    //     case 'reverse':
-    //         reverseStringFunction(option)
-    //         break;
-    //     case 'transform':
-    //         break;
-    //     case 'outputFile':
-    //         break;
-    //     case 'convertFromFile':
-    //         break;
-    //     case 'convertToFile':
-    //         break;
-    //     default:
-    //         return;
-    // }
+let getAction = (action) => {
+    return action;
 }
 
 let getFile = (file) => {
-    console.log(file);
+    return file;
+}
+
+let getString = (string) => {
+    return string;
 }
 
 let reverseStringFunction = (string) => {
     // TODO - reverse string data from process.stdin to process.stdout
     console.log(string.split('').reverse().join(''));
-
-program
-    .command('reverse <str>')
-    .description('reverse string data from process.stdin to process.stdout')
-    .action((str) => {
-        reverseStringFunction(str);
-    })
 }
 
-/*
-program
-    .command('transform <str>')
-    .description('convert data from process.stdin to upper-cased data on process.stdout')
-    .action((str) => {
-        console.log('??');
-    })
 
 program
-    .command('outputFile <filePath>')
-    .description('pipe the given file provided by --file option to process.stdout')
-    .action((filePath) => {
-        console.log('??');
-    })
+    .version('0.1.0')
+    .option('-a, --action <action>', 'Action to be performed', getAction)
+    .option('-s, --string [string]', 'String', getString)
+    .option('-f, --file [file]', 'File', getFile)
+    .parse(process.argv);
 
-program
-    .command('convertToFile <filePath>')
-    .description('pipe the given file provided by --file option to process.stdout')
-    .action((filePath) => {
-        console.log('??');
-    })
+// const action = process.argv[2].split('=')[1];
+// const file = process.argv[3].split('=')[1];
+// console.log(action, file);
 
-program
-    .command('convertFromFile <filePath>')
-    .description('convert file provided by --file option from csv to json and output data to a result file with the same name but json extension')
-    .action((filePath) => {
-        console.log('??');
-    })
-*/
+// program.parse(process.argv);
 
-program
-  .version('0.1.0')
-//   .arguments('<action> [file]')
-  .option('-a, --action <action> [string]', 'Action to be performed', getAction)
-  .option('-f, --file <file>', 'File', getFile)
-//   .action( (action, file) => {
-//     cmdValue = action;
-//     envValue = file;
-//  });
-//   .parse(process.argv);
+switch(program.action) {
+    case 'reverse':
+        reverseStringFunction(program.string)
+        break;
+    case 'transform':
+        break;
+    case 'outputFile':
+        break;
+    case 'convertFromFile':
+        break;
+    case 'convertToFile':
+        break;
+    default:
+        return;
+}
 
-program.parse(process.argv);
-// console.log('action:', cmdValue);
-// console.log('file:', envValue);
+// if (!program.args.length) {
+//     program.help();
+// } 
 
-if (!program.args.length) {
-    program.help();
-} 
-
+// https://nodejs.org/api/process.html#process_process_stdin
 // https://tj.github.io/commander.js/
 // https://medium.freecodecamp.org/writing-command-line-applications-in-nodejs-2cf8327eee2
-// http://cruft.io/posts/node-command-line-utilities/
