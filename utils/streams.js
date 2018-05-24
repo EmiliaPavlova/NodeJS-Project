68705383d69e212b1b4795a3065cf01b596a81f4
Emilia Pavlova <emilia.pavlova@gmail.com>
2018-05-24 16:43:07 +0300

    no check for wrong file name

--- utils/streams.js
+++ utils/streams.js
@@ -1,9 +1,11 @@
 #!/c/Program Files/nodejs/env node
 
-const csv = require("csvtojson");
 const fs = require('fs');
 const program = require('commander');
 const stream = require('stream');
+const Converter = require("csvtojson").Converter;
+
+const path = './data';
 
 let getAction = (action) => {
     return action;
@@ -17,8 +19,12 @@ let showHelp = () => {
     program.help();
 }
 
+let getFilePath = (file) => {
+    return `./data/${file}`;
+}
+
 let reverseStringFunction = () => {
-    console.log('function to reverse string data from process.stdin to process.stdout');
+    // console.log('function to reverse string data from process.stdin to process.stdout');
 
     process.stdin
         .pipe(new stream.Transform({
@@ -30,7 +36,7 @@ let reverseStringFunction = () => {
 }
 
 let transformDataToUpperCase = () => {
-    console.log('function to convert data from process.stdin to upper-cased data on process.stdout.');
+    // console.log('function to convert data from process.stdin to upper-cased data on process.stdout.');
 
     process.stdin
         .pipe(new stream.Transform({
@@ -42,9 +48,9 @@ let transformDataToUpperCase = () => {
 }
 
 let outputFile = () => {
-    console.log('function that will use fs.createReadStream() to pipe the given file provided by --file option to process.stdout');
+    // console.log('function that will use fs.createReadStream() to pipe the given file provided by --file option to process.stdout');
 
-    const pathFile = `./data/${program.file}`;
+    const pathFile = getFilePath(program.file);
     fs.createReadStream(pathFile)
         .pipe(new stream.Transform({
             transform: (chunk, encoding, callback) => {
@@ -55,20 +61,29 @@ let outputFile = () => {
 }
 
 let convertFromFile = () => {
-    console.log('convert file provided by --file option from csv to json and output data to process.stdout. Function should check that the passed file name is valid');
+    // console.log('convert file provided by --file option from csv to json and output data to process.stdout. Function should check that the passed file name is valid');
 
-    const pathFile = `./data/${program.file}`;
+    const pathFile = getFilePath(program.file);
+    fs.createReadStream(pathFile).pipe(new Converter({})).pipe(process.stdout);
+
+    /*
     csv()
         .fromFile(pathFile)
-        .then((jsonArrayObj) => { //when parse finished, result will be emitted here.
-            console.log(jsonArrayObj);
+        .then((jsonObj)=>{
+            console.log(jsonObj);
         })
-    // fs.createReadStream(pathFile)
-    //     .pipe(fs.createWriteStream(file + '.json'));
+    */
 }
 
 let convertToFile = () => {
-    console.log('convert file provided by --file option from csv to json and output data to a result file with the same name but json extension. Function should check that the passed file name is valid and use fs.createWriteStream additionally');
+    // console.log('convert file provided by --file option from csv to json and output data to a result file with the same name but json extension. Function should check that the passed file name is valid and use fs.createWriteStream additionally');
+
+    const pathFile = getFilePath(program.file);
+    const index = pathFile.lastIndexOf('.csv');
+    const newFile = `${pathFile.substring(0, index)}.json`;
+    const writeStream = fs.createWriteStream(newFile);
+
+    fs.createReadStream(pathFile).pipe(new Converter({})).pipe(writeStream);
 }
 
 program
@@ -96,14 +111,15 @@ switch(program.action) {
         convertToFile();
         break;
     default:
-        return;
+        console.error('Error: no such command!');
+        process.exit(1);
 }
 
-console.log('program.args', program.args);
+// console.log('program.args', program.args);
 
-//if (!program.args.length) {
- //   console.log('Wrong input: no arguments provided');
- //   program.help();
-//} 
+// if (!program.args.length) {
+//    console.log('Wrong input: no arguments provided');
+//    program.help();
+// } 
 
 // https://medium.freecodecamp.org/node-js-streams-everything-you-need-to-know-c9141306be93
