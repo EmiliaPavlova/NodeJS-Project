a599a082e79c313d806e4cd414314c7f31995020
Emilia Pavlova <emiliya_pavlova@epam.com>
2018-06-11 17:35:40 +0300

    add echo-server.js

--- server/echo-server.js
+++ server/echo-server.js
@@ -5,12 +5,24 @@ const port = 3003;
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, {
-        'Content-Type': 'text/plain'
+        'Content-Type': 'text/html'
     });
-    res.statusCode = 200;
-    res.end('Done');
+
+    req.on('error', (error) => {
+        console.log(error);
+    });
+    res.on('error', (error) => {
+        console.log(error);
+    });
+
+    req.on('data', (message) => {
+        res.write(message);
+    });
+    req.on('end', () => {
+        res.end();
+    });
 });
 
 server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}`);
+    console.log(`Server running at http://${hostname}: ${port}`);
 });
